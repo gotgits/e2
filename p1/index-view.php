887d73aedb5a5d2ba9be049d89e0c41af519bd08
Rose Mikan <rom6968@g.harvard.edu>
2021-09-20 19:32:41 +0000

    Updates testing for image url from index.php

--- p1/index-view.php
+++ p1/index-view.php
@@ -2,7 +2,8 @@
 
 <head>
     <title>Project 1</title>
-    <meta charset='utf-8' </title> <link href=data". rel=icon>
+    <meta charset='utf-8' </title>
+    <link href=data". rel=icon>
 </head>
 
 <body>
@@ -15,6 +16,8 @@
     <ul>
         <li>...</li>
     </ul>
+    <h2>Testing</h2>
+    <img src="https://e2p1.metrognome.me/e2/p1/sketch1idea_IMG-3446.jpg" alt="">
 </body>
 
 </html>
