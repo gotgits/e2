dcfb73cfc9b8ddeefa2b84866a00151077b9dc8c
Rose Mikan <rom6968@g.harvard.edu>
2021-10-06 01:33:57 +0000

    Final commit before submitting project 1

--- p1/index-view.php
+++ p1/index-view.php
@@ -3,28 +3,40 @@
 
 <head>
     <meta charset='UTF-8' />
-    <title>Project 1 – 5x5 game simulation</title>
+    <title>Project 1 – Finish Line Game Simulation</title>
     <link href="data" . rel=icon>
 </head>
 
-<body>
-    <?php
-    //include CSS Style Sheet
-    echo "<link rel='stylesheet' type='text/css' href='game_style.css' />"; ?>
-    <h1>Project 1 - 5x5 Game Simulation</h1>
-    <h2>Mechanics</h2>
-    <!-- <ul>
-        <li class="playerA">Player A moves <?php echo $playerA ?>times to reach the end</li>
-        <li class="playerB">Player B moves <?php echo $playerB ?>times to reach the end </li>
-        <li class="winner">The winner is <?php echo $winner ?></li>
-    </ul> -->
-    <h2>Results</h2>
-    <ul>
-        <li class="playerA"><a href=" "></li>
-        <li class="playerB"><a href=" "></li>
-        <li class="winner"><a href=" "></li>
-        <li class="total"><a href=" "></li>
-    </ul>
+<body class="background">
+    <main>
+        <?php
+        echo "<link rel='stylesheet' type='text/css' href='game_style.css' />"
+        ?>
+
+        <h1>Index-1 – Project 1 - Finish Line</h1>
+        <h2>Mechanics</h2>
+        <ol>
+            <li>Each player gets a series of random numbers that represent spaces on a board</li>
+            <li>Each players numbers are accumulated as if they were moving across the board</li>
+            <li>The first player's total that adds up to 25 wins.
+        </ol>
+        <h3>Showing round 1 play</h3>
+        <ul>
+            <li class="playerA">Player A moves <?php echo $totalA ?> spaces</li>
+            <li class="playerB">Player B moves <?php echo $totalB ?> spaces</li>
+            <li class="winner">Round Winner – <?php echo $roundWinner ?></li>
+        </ul>
+        <br>
+        <h2>Results</h2>
+
+        <?php foreach ($results as $turn => $details) { ?>
+        <ul>
+            <li class="playerA">Number of turns taken by Player A: <?php echo $details['playerATurnCount'] ?></li>
+            <li class="playerB">Number of turns taken by Player B: <?php echo $details['playerBTurnCount'] ?></li>
+            <li class="winner">You reached the finish first, <?php echo $details['winner'] ?></li>
+        </ul>
+        <?php } ?>
+    </main>
 </body>
 
 </html>
--- p1/index.php
+++ p1/index.php
@@ -1,80 +1,50 @@
 <?php
-# controller 
-
-$board = array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25); #prevent error beyond end of array
-shuffle($board);
-var_dump($board);
-$move = [0, 1];
-# divide the board into even and odd positions
-# variation 4 - dynamic variables
-foreach ($board as $key => $move) {
-    if ($key % 2 == 0) {
-        $moveTo = 'evenSpot';
-    } else {
-        $moveTo = 'oddSpot';
-    }
-    $$moveTo[] = array_pop($board);
+# Iterate for selection of random number for each player for each round of play
+for ($i = 0; $i < $turn = rand(1, 25); $i++) {
+    // Turn to move Player A
+    $playerAturn = $turn;
+    $totalA = $turn;
+    $totalA = $playerAturn;
 }
 
+for ($j = 0; $j < $turn = rand(1, 25); $j++) {
+    # move Player B
+    $playerBturn = $turn;
+    $totalB = $playerBturn;
+}
 
-// assign unique position array elements to odd numbers
+if ($totalA > $totalB) {
+    $roundWinner = 'Player A is Ahead, Congratulations!';
+} elseif ($totalA < $totalB) {
+    $roundWinner = 'Player B is Ahead, Congratulations!';
+} else
+    $roundWinner = 'tie round, keep going.';
 
+# Accumulate total moves by each player
+$playerATotal = 0;
+$playerATurnCount = 0;
+while ($playerATotal < 25) {
+    $playerATotal = $playerATotal + rand(1, 25);
+    $playerATurnCount++;
+}
 
-// $spots = array($begin, $magic, $bonus1, $curse, $mystic, $wild, $bonus2, $beware, $goal);
-// multi-dimensional associative array key value pairs for unique spots
-/* NOTE: find correct syntax for error free multiplication and division */
-$spot_values = [
-    'begin' => [
-        'count' => 1,
-        'value' => 1
-    ],
-    'magic' => [
-        'count' => 5,
-        'value' => 10
-    ],
-    'bonus1' => [
-        'count' => 7,
-        'value' => +3 < 12
-    ], 'curse' => [
-        'count' => 9,
-        'value' => -2
-    ],
-    'mystic' => [
-        'count' => 13,
-        'value' => -1 < 13 or +1 > 13 or -5 > 7 or +5 < 19
-    ], 'wild' => [
-        'count' => 17,
-        'value' => -2 > 10 or +2 < 20 or +1 or -1
-    ], 'bonus2' => [
-        'count' => 19,
-        'value' => +2
-    ],
-    'beware' => [
-        'count' => 21,
-        'value' => -10
-    ],
-    'goal' => [
-        'count' => 25,
-        'value' => +1
-    ]
+$playerBTotal = 0;
+$playerBTurnCount = 0;
+while ($playerBTotal < 25) {
+    $playerBTotal = $playerBTotal + rand(1, 25);
+    $playerBTurnCount++;
+}
+
+# Compare player total moves to decide winner
+if ($playerATurnCount < $playerBTurnCount) {
+    $winner = 'Player A Wins';
+} else $winner = 'Player B Wins!';
+
+# Display results
+$results[] = [
+    'playerATurnCount' => $playerATurnCount,
+    'playerBTurnCount' => $playerBTurnCount,
+    'winner' => $winner,
 ];
-
-# Random number turn for loop to iterate to next turn until reach 25
-// $playerA = $move[rand(0, 25)];
-// $playerB;
-
-$turns;
-$winner;
-$playerA = 0;
-// $playerB = 0;
-
-# totalMoves = 0;
-
-# Decide the winner
-if ($playerA == '$move[rand(0, 25)]') {
-    $winner = 'Player A';
-} else {
-    $winner = 'Player B';
-}
 
 require 'index-view.php';
