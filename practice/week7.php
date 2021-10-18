16ea99bd59f5c0cb8d0c9335439b8ff2ad547cba
Rose Mikan <rom6968@g.harvard.edu>
2021-10-18 17:32:30 +0000

    Practice update

--- practice/week7.php
+++ practice/week7.php
@@ -1,7 +1,8 @@
 <?php
 
-// $example = (5 > 10) ? 9 : 10;
-// var_dump($example); # 10
+$example = (5 > 10) ? 9 : 10;
+echo $example; # 10
+echo "<br><br>";
 
 class Person
 {
@@ -9,7 +10,7 @@ class Person
     public $firstName;
     public $lastName;
     public int $age;
-
+    public $getFullName;
 
     # Methods
     public function __construct($firstName, $lastName, $age)
@@ -18,10 +19,14 @@ class Person
         $this->lastName = $lastName;
         $this->age = $age;
     }
-    // public function getFullName()
-    // {
-    //     $this->getFullName = '$this->firstName ' . '$this->lastName';
-    // }
+    public function getFullName()
+    {
+        $getFullName = "$this->firstName $this->lastName";
+        $this->getFullName = $getFullName;
+        if ($this->getFullName) {
+        }
+        return $this->getFullName;
+    }
     public function getClassification()
     {
         if ($this->age >= 18) {
@@ -32,8 +37,36 @@ class Person
 }
 $person = new Person('John', 'Harvard', 45);
 echo $person->firstName;
-echo "\n";
+echo "<br>";
 // echo $person->lastName;
+// echo "<br>";
 // echo $person->age;
-// echo $person->getFullName();
+echo $person->getFullName();
+echo "<br>";
 echo $person->getClassification();
+echo "<br>";
+
+
+$example1 = vowelCount('apple'); # Should yield 2
+$example2 = vowelCount('lynx'); # Should yield 0
+$example3 = vowelCount('hi'); # Should yield 1
+$example4 = vowelCount('mississippi'); # Should yield 4
+
+function vowelCount($word)
+{
+    $vowels = ['a', 'e', 'i', 'o', 'u'];
+    $vowelCount = 0;
+    # https://www.php.net/manual/en/language.operators.string.php
+    $words = str_split($word, 1);
+    foreach ($words as $character) {
+        # https://www.php.net/manual/en/function.in-array.php
+        if (in_array($character, $vowels)) {
+            $vowelCount++;
+        }
+    }
+    return $vowelCount;
+}
+echo "<br>apple has $example1 vowel(s)";
+echo "<br>lynx has $example2 vowel(s)";
+echo "<br>hi has $example3 vowel(s)";
+echo "<br>mississippi has $example4 vowel(s)";
