895b1ef72274cb9b1294a08c4b4efc71471a89fc
Rose Mikan <rom6968@g.harvard.edu>
2021-11-16 01:13:51 +0000

    preliminary p3 in p0

---
+++ p0/index-view.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Summed up</title>
+    <link href='index.css' rel='stylesheet'>
+</head>
+
+<body>
+    <main>
+        <h2>All Summed Up</h2>
+        <h3>Results</h3>
+        <?php foreach ($results as $rounds => $details) { ?>
+        <dl>
+            <dt>Round # <?php echo $rounds ?></li>
+            <dd class="rounds">Player gets:
+                <?php echo $details['playerTurn']." &rarr; Board position ".$details['playerSum'] ?>
+                </li>
+            <dd class="rounds">Competitor gets:
+                <?php echo $details['competitorTurn']. " &rarr; Board position ".$details['competitorSum'] ?>
+                </li>
+                <?php } ?>
+        </dl>
+
+        <div class="results">Goal reached.<br>Player's final position on board <?=$playerSum?><br> Competitor's final
+            position on board <?=$competitorSum?>
+            <p class="winner">The Winner is <em><?=$winner?></em></p>
+        </div>
+
+    </main>
+</body>
+
+</html>
---
+++ p0/index.php
@@ -0,0 +1,60 @@
+ <?php
+    // Defining variables
+    $playerSum = 0;
+    $competitorSum = 0;
+    $goal = 25;
+
+    // while loop running until $sum1 or $sum2 >= $goal
+    $results = [];
+    $rounds = [];
+
+    while ($playerSum < 25 && $competitorSum < $goal) {
+        // random dice, safing result into variables
+        $playerTurn = random_int(1, 5);
+        $competitorTurn = random_int(1, 5);
+        // $sum1 is increased by $playerTurn
+        $playerSum += $playerTurn;
+        // checking round results for goal (winner Player)
+        if ($playerSum >= $goal) {
+            // echo $playerTurn." ".$playerSum;
+        }
+        // $competitorTurn is increased by $competitorTurn
+        $competitorSum += $competitorTurn;
+        // checking round results for goal(winner Competitor)
+        if ($competitorSum >= $goal) {
+            // echo $competitorTurn." ".$competitorSum;    
+        }
+        // Individual round results
+            // echo "Player turn $playerTurn Player Round Sum: $playerSum <br>
+            // Competitor turn $competitorTurn Competitor Round Sum: $competitorSum<br><br>";
+            
+    
+
+        // Round results of both players
+        // $round[]=$results;
+
+        // }
+        // When goal is met Winner
+        if ($winner = $playerSum >= $goal || $competitorSum >= $goal) {
+            if ($playerSum >= $goal) {
+                $winner = 'Player';
+            } else {
+                $winner = 'Competitor';
+            }
+        }
+        $round = count($rounds) +1;
+            array_push(
+                $results,
+                    array(
+                    'rounds' => $rounds,
+                    'playerTurn' => $playerTurn,
+                    'playerSum' => $playerSum,
+                    'competitorTurn' => $competitorTurn,
+                    'competitorSum' => $competitorSum,
+                    'winner' => $winner,
+                    )
+                );
+            
+        }
+        require 'index-view.php';
+ 
