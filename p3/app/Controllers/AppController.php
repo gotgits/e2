b8e38d8e5a3613368cb7fcc4bfa800533e96cadd
Rose Mikan <rom6968@g.harvard.edu>
2021-12-17 18:23:32 +0000

    Latest

--- p3/app/Controllers/AppController.php
+++ p3/app/Controllers/AppController.php
@@ -16,23 +16,23 @@ class AppController extends Controller
         $player_turns = $this->app->old('player_turns');
         $opponent_turns = $this->app->old('opponent_turns');
         $winner = $this->app->old('winner');
-        $results = $this->app->old('results');
+        $round = $this->app->old('round');
         $timestamp = $this->app->old('timestamp');
         $id_turns = $this->app->old('id_turns');
         
 
         if ($timestamp) {
-            $results = $this->app->db()->findByColumn('rounds', 'timestamp', '=', $timestamp);
-            $results = $results[0]; # Narrow it down to the first row returned
+            $round = $this->app->db()->findByColumn('rounds', 'timestamp', '=', $timestamp);
+            $round = $round[0]; # Narrow it down to the first row returned
 
             # Step 1) Create an array of each turn
-            $results['player_turns'] = explode(',', $results['player_turns']);
-            $results['opponent_turns'] = explode(',', $results['opponent_turns']);
-            $results['id_turns'] = explode(',', $results['id_turns']);
+            $round['player_turns'] = explode(',', $round['player_turns']);
+            $round['opponent_turns'] = explode(',', $round['opponent_turns']);
+            $round['id_turns'] = explode(',', $round['id_turns']);
 
             # Step) Create sub array of details of each turn
             $turns_player = [];
-            foreach ($results['player_turns'] as $turn) {
+            foreach ($round['player_turns'] as $turn) {
                 $turn = explode(',', $turn);
                 if (!isset($turn[1])) {
                     $turn[1]="";
@@ -42,7 +42,7 @@ class AppController extends Controller
                 $turns_player[] = $turn;
             }
             $turns_opponent = [];
-            foreach ($results['opponent_turns'] as $turn) {
+            foreach ($round['opponent_turns'] as $turn) {
                 $turn = explode(',', $turn);
                 if (!isset($turn[1])) {
                     $turn[1]="";
@@ -53,7 +53,7 @@ class AppController extends Controller
             }
 
             $turns_id = [];
-            foreach ($results['id_turns'] as $turn) {
+            foreach ($round['id_turns'] as $turn) {
                 $turn = explode(',', $turn);
                 if (!isset($turn[1])) {
                     $turn[1]="";
@@ -63,18 +63,18 @@ class AppController extends Controller
                 $turns_id[] = $turn;
             }
 
-            $results['player_turns'] = $turns_player;
-            $results['opponent_turns'] = $turns_opponent;
-            $results['id_turns'] = $turns_id;
+            $round['player_turns'] = $turns_player;
+            $round['opponent_turns'] = $turns_opponent;
+            $round['id_turns'] = $turns_id;
         }
 
-        # show the results on page
+        # show the round on page
         return $this->app->view('index', [
             'player_name' => $player_name,
             'timein' => $timein,
             'player_saved' => $player_saved,
             'competitor' => $competitor,
-            'results' => $results,
+            'round' => $round,
             'player_turns' => $player_turns,
             'opponent_turns' => $opponent_turns,
             'id_turns' => $id_turns,
@@ -90,6 +90,7 @@ class AppController extends Controller
         $opponent_sum = 0;
         $opponent_turns = [];
         $timestamp = date('Y-m-d H:i:s');
+        $id = $this->app->param('id');
         $turn = 0;
         $id_turns = [];
        
@@ -119,7 +120,8 @@ class AppController extends Controller
 
         # Build an array that contains full details of a "round" of the game
         # Details include each "turn" for each player/opponent that include points/sums
-        $results = [
+        $round = [
+                'id' => $id,
                 'player_turns' => $player_turns,
                 'opponent_turns' => $opponent_turns,
                 'id_turns' => $id_turns,
@@ -171,42 +173,11 @@ class AppController extends Controller
 
     public function round()
     {    
+        $id = $this->app->param('id');        
+        $round = $this->app->db()->findById('rounds', $round['id']);
+        $round['id'] = explode(',', $round[$round['id']]); 
 
-        
-        $results =[];
-        $id = $this->app->param('id'); 
-        $results['id'] = explode(',', $results['id']);
-        $idAsString = '';
-        foreach ($id as $turn) {
-            $turnAsString = implode(",", $turn); # Convert each turn to a string
-            $idsAsString .= $turnAsString . " |  ";
-        }
-        $idAsString = trim($idsAsString, ' |  '); # Remove last
-        
-        $round = $this->app->db()->findById('rounds', $results['id']);
-        $round['id'] = explode(',', $round[$results['id']]); 
-        
-
-        
-        // $id['round'] = explode(',', $id['round']);
-        
-
-            
-            dump($results['id']);
-            // dump($id);
-            dump($timestamp);
-            // dump($results['id_turns']);
-            // dump($results['timestamp']);
-        
-            # show the results on page
-        // return $this->app->view('round', [
-        //     'id' => $id,
-        //     'timestamp' => $timestamp,
-        //     // 'results' => $results,
-        //     // 'player_turns' => $player_turns,
-        //     // 'opponent_turns' => $opponent_turns,
-        //     // 'winner' => $winner,
-        // ]);
+        dump($round);
         
     }
 
